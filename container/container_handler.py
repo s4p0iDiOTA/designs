77b4ea1aad83caa4c12dbc41f34a3dc6a8f42137
Alejandro G.R. <galreb2002@gmail.com>
2025-02-18 01:04:33 +0000

    Merge remote-tracking branch 'origin/add_more_container_fuctionality' into add_more_container_fuctionality

--- container/container_handler.py
+++ container/container_handler.py
@@ -196,6 +196,7 @@ def horiz_align_containers_in_page(page: AlbumPage, work_area_width: float, alig
                 current_x += container.width + space
 
     page.containers = [item for row, _ in rows for item in row]
+<<<<<<< HEAD
 
 # Align the containers vertically within the page.
 def vert_align_containers_in_page(page: AlbumPage, alignment): # alignment can be: "top", "middle", or "bottom". 
@@ -232,6 +233,9 @@ def vert_align_containers_in_page(page: AlbumPage, alignment): # alignment can b
     page.containers = [item for row, _ in rows for item in row]
       
 
+=======
+    
+# TODO: Add function for vertical alignment of the containers in the page.    
 
     
 # TODO: Add function for vertical alignment of the containers in the page.    
